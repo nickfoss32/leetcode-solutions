13a437160b2c40d34239476121930c4ad4f51521
Nicholas Foss <nickfoss32@icloud.com>
2022-03-13 09:54:34 -0400

    Added build infrastructure.

---
+++ scripts/build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+## Environment ##
+REPO_ROOT=`git rev-parse --show-toplevel`
+
+################################################################################
+# Help                                                                         #
+################################################################################
+Help()
+{
+	echo "Script for building leetcode solutions."
+   echo
+   echo "Syntax: ./build.sh [-v|-h]"
+   echo "Options:"
+	echo "-v|--verbose Enables verbose output."
+   echo "-h|--help	Prints this usage."
+   echo
+}
+
+################################################################################
+# echo_cmd                                                                     #
+################################################################################
+# echo_cmd()
+# {
+#    if [[ -v VERBOSE ]]; then printf '%s\n' "${*}"; "${@}"; else "${@}"; fi; echo
+# }
+
+################################################################################
+################################################################################
+# Main program                                                                 #
+################################################################################
+################################################################################
+
+## Parse command line arguments ##
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+   key="$1"
+
+   case $key in
+      -v|--verbose)
+         VERBOSE=1
+         shift
+         ;;
+      -h|--help)
+         Help
+         shift
+         exit
+         ;;
+      *)    # unknown option
+         shift
+         ;;
+   esac
+done
+
+declare -a targets=("x86_64-apple-darwin")
+
+for target in "${targets[@]}"; do
+   ## Create build directories ##
+   mkdir -p $REPO_ROOT/build/$target
+
+   ## Configure and generate build environment with CMake ##
+   cmake --preset=$target -S $REPO_ROOT -B $REPO_ROOT/build/$target -DCMAKE_BUILD_TYPE=RelWithDebInfo
+
+   ## build ##
+   cmake --build $REPO_ROOT/build/$target -- VERBOSE=$VERBOSE
+done
